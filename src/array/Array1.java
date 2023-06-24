74552056df82b5ea26b385f5996d8f7b081350db
velibkolay <veliberkaykolay@gmail.com>
2023-06-24 17:43:10 +0200

    solve array1

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array1 {
 
     public static boolean firstLast6(int[] nums) {
@@ -14,4 +16,20 @@ public class Array1 {
         return new int[]{3, 1, 4};
     }
 
+    public boolean commonEnd(int[] a, int[] b) {
+        return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+    }
+
+    public int sum3(int[] nums) {
+        return Arrays.stream(nums).sum();
+    }
+
+    public int[] rotateLeft3(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[(i + 1) % nums.length - 1];
+        }
+        return result;
+    }
 }
